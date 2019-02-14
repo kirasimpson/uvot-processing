16def8edc04ea2c62e36c1eb5a95902ff44d13d2
kirasimpson <kira.simpson1984@gmail.com>
2019-02-14 16:07:26 -0500

    Update auto_stack_color.py
    At this point many changes have been made, but the point of this code is still to download, stack, and process data and produce photometry plots.

--- auto_stack_color.py
+++ auto_stack_color.py
@@ -14,214 +14,183 @@ import aplpy
 import uvot_deep
 import config_uvot_mosaic
 import offset_mosaic
-
-def auto_color():
-	gal_list = np.loadtxt("modified_galaxy_list", dtype = 'str').tolist()
-	unprocessed_gals = open('unprocessed_gals', 'w+')
-	#up_gals = list()
-	for gal in gal_list:
-		os.chdir('..')
-		#cd into existing directory, then make a new one for the stuff we're working on
-
-		dir_exists = glob.glob(gal)
-		if len(dir_exists) == 0:
-			os.mkdir(gal)
-
-		os.chdir(gal)
-
-		#this may not stay for later versions of this tool, but it's useful to me for now
-		processing_done = glob.glob(gal+"_image.png")
-		if len(processing_done) > 0: 
-			print("* " + gal + "_image.png already created, moving on to next item...")
-			continue				 
-
-		'''processing is done if the .png file was created, so if it's there, then just move on
-		   this is temporary, in case it doesn't finish a list of galaxies and the script needs to be run again'''
-
-		#make the download script and store it in the new directory here?
-		os.system('browse_extract_wget.pl table=swiftmastr position=' + gal + ' radius=5 fields=obsid,start_time outfile=data.dat')
-
-		idl = pIDLy.IDL()
-
-		#run browse_extract with all of the parameters needed to make data.dat
-
-		#filename.open() new file here that will hold all the wget commands
-		download_scr = open('download.scr', 'w+')
-
-		obslist = np.loadtxt('data.dat', dtype = 'str', delimiter = '|', comments = 'S', skiprows = 2, usecols = (1,2)).tolist()
-		id_list = list()
-
-		#condition that handles cases where HEASARC query returns only one row
-		if len(obslist[0]) > 2:
-			#print(obslist)
-			obsid = obslist[0]
-			starttime = obslist[1]
-			start_month = starttime[0:7]
-			start_month = start_month.replace('-','_')
-			id_list.append(obsid)
-			wget_uvot = "wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/" + start_month + '//' + obsid + "/uvot/"
-			wget_auxil = "wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/" + start_month + '//' + obsid + "/auxil/"
-			download_scr.write(wget_uvot)
-			download_scr.write("\n")
-			download_scr.write(wget_auxil)
-			download_scr.write("\n")
-		else:
-			for i in range(len(obslist)):
-				#print(obslist[i])
-				[obsid, starttime] = obslist[i]
-		
-				start_month = starttime[0:7]
-				start_month = start_month.replace('-','_')
-				id_list.append(obsid)
-		
-	#	string addition to make wget commands for data download
-				wget_uvot = "wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/" + start_month + '//' + obsid + "/uvot/"
-				wget_auxil = "wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/" + start_month + '//' + obsid + "/auxil/"
-				download_scr.write(wget_uvot)
-				download_scr.write("\n")
-				download_scr.write(wget_auxil)
-				download_scr.write("\n")
-
-		#check for me to know if download has been done already
-		#ooh problem, it will stop processing for all galaxies, I want it to pass on to the data processing stage
-		#gotta fix that
-		#shit
-		download_scr.close()
-
-		obs_folder_exists = glob.glob("000*")
-		if len(id_list) == len(obs_folder_exists):
-			print("* Download already done, moving on to data processing...")
-			#make data processing chunk into a function
-		else:
-			#make download script executable
-			st = os.stat('download.scr')
-			os.chmod('download.scr', st.st_mode | stat.S_IEXEC)
-			print("* running download script for "+gal)
-			os.system('download.scr')
-
-			#unzip all the downloaded data
-			os.system('gunzip */*/*.gz')
-			os.system('gunzip */*/*/*.gz')
+import swift_db_query
+import query_heasarc
+import download_heasarc
 
 
-		Name_Prefix = gal
+def image_processing():
+		#test this out, include checking for new data
+		#galaxies with new data...how do I make a new list?
+	idl = pIDLy.IDL()
 
-		filt_list = ['w2','m2','w1']
-		filter_list = list()
-		for filt in filt_list:
-			filt_data_exists = glob.glob('*/*/image/*'+filt+'*')
-			if len(filt_data_exists) > 0: #glob useful for wildcards here, could be used to check images before processing anyway
-				filter_list.append(filt)
 
-		if len(filter_list) == 1:
-			print('Data only found for filter '+filter_list[0]+", processing will not continue. Please check to make sure data for each filter exists.")
-			print('Moving on to next object...')
-			unprocessed_gals.write(gal)
-			unprocessed_gals.write('\n')
+	cr_img_exists = glob.glob('*cr.fits') #turn this into an if/and where there's a second condition that concerns a second data download...hm. what could I check?
+	if len(cr_img_list) == 0 or inter_list:
+		try:
+			uvot_deep.uvot_deep(id_list, Name_Prefix+'_', ['w2','m2','w1']) #tool made by Lea Hagen
+		except FileNotFoundError:
+			print('* Required files not made out of uvot_deep for '+gal+', moving on...')
+			unprocessed_gals.write(gal + '\n')
+			why_bypass.write(gal+': required files not made out of uvot_deep'+'\n')
 			continue
-		elif len(filter_list) == 2:
-			print('Data only found for filters '+filter_list[0]+" and "+filter_list[1]+", processing will not continue. Please check to make sure data for each filter exists.")
-			print('Moving on to next object...')
-			unprocessed_gals.write(gal)
-			unprocessed_gals.write('\n')
+		except OSError:
+			print('Too many open files error thrown, whatever that means. Moving on...') #sass maybe not good, maybe chill
+			unprocessed_gals.write(gal + '\n')
+			why_bypass.write(gal+': too many open files error' + '\n')
 			continue
+	else:
+		print("* uvot_deep already completed successfully, moving on!")
+		input()
 
-		uvot_deep.uvot_deep(id_list, Name_Prefix+'_', ['w2','m2','w1']) #tool made by Lea Hagen
 
-		#filter list in most cases will be the same, but this section will take care of cases where there's no data for one of the UV filter
-
-		#now check to make sure all the deep images needed for processing were made using glob
-		deep_image_list = list()
-		for filt in filter_list:
-			deep_image_exists = glob.glob('*'+filt+'_sk.fits')
-			if len(deep_image_exists) > 0: #glob useful for wildcards here, could be used to check images before processing anyway
-				deep_image_list.append(filt)
-
-		if len(filter_list) == 1:
-			print('Deep image only created for '+filter_list[0]+", processing will not continue. Please check to make sure data for each filter exists.")
-			print('Moving on to next object...')
-			unprocessed_gals.write(gal)
-			unprocessed_gals.write('\n')
-			continue
-		elif len(filter_list) == 2:
-			print('Deep images only created for '+filter_list[0]+" and "+filter_list[1]+", processing will not continue. Please check to make sure data for each filter exists.")
-			print('Moving on to next object...')
-			unprocessed_gals.write(gal)
-			unprocessed_gals.write('\n')
-			continue
-
+	try:
 		if True:
 			offset_mosaic.offset_mosaic(gal+'_', gal+'_offset_', filter_list,
 										min_exp_w2=150, min_exp_m2=150, min_exp_w1=150, restack_id=True)
+	except IndexError:
+		print("* index 0 is out of bounds for axis 0 with size zero")
+		unprocessed_gals.write(gal+'\n')
+		why_bypass.write(gal+': offset mosaic problem, index 0 is out of bounds for axis 0 with size zero'+'\n')
+		continue
 
-		#list of uv filters, can be changed if someone needs optical filter images as well.
-		#filter_list = ['w1', 'm2', 'w2']
+	#list of uv filters, can be changed if someone needs optical filter images as well.
+	#filter_list = ['w1', 'm2', 'w2']
 
-		for filt in filter_list:
+	Name_Prefix = gal
 
-			#taken from Lea's code stack_uvot
-			with fits.open(Name_Prefix + "_" + filt + '_cr.fits') as hdu_cr: #cr = count rate
+	for filt in filter_list:
 
-				pix_clip = sigma_clip(hdu_cr[0].data, sigma=2.5, iters=3)
-				cr_mode = biweight_location(pix_clip.data[~pix_clip.mask])
-				#print('cr_mode: ', cr_mode)
+		#taken from Lea's code stack_uvot
+		with fits.open(Name_Prefix + "_" + filt + '_cr.fits') as hdu_cr: #cr = count rate
 
-				hdu_cr[0].data -= cr_mode
+			pix_clip = sigma_clip(hdu_cr[0].data, sigma=2.5, iters=3)
+			cr_mode = biweight_location(pix_clip.data[~pix_clip.mask])
+			#print('cr_mode: ', cr_mode)
 
-				hdu_cr.writeto(gal + '_offset_' + filt + '_cr_bgsub.fits', overwrite=True)
+			hdu_cr[0].data -= cr_mode
 
-			idl.pro('image_shift', Name_Prefix) #idl procedure that aligns all the images
+			hdu_cr.writeto(gal + '_offset_' + filt + '_cr_bgsub.fits', overwrite=True)
 
-		idl.close()
+		idl.pro('image_shift', Name_Prefix) #idl procedure that aligns all the images
 
-			#replaces mkcolor idl script (credits to Lea)
-			# make RGB image
+	idl.close()
 
-		#if no data for the filter, gotta skip over the image that would be created
-		im_r = gal + '_offset_w1_cr_bgsub.fits'
-		im_g = gal + '_offset_m2_cr_bgsub.fits'
-		im_b = gal + '_offset_w2_cr_bgsub.fits'
+		#replaces mkcolor idl script (credits to Lea)
+		# make RGB image
 
-		#another block to handle the lack of data for one filter
-		# im_list = [im_r, im_g, im_b]
-		# image_list = list()
-		# for image in im_list:
-		# 	if os.path.isfile(image):
-		# 		image_list.append(image)
+	#if no data for the filter, gotta skip over the image that would be created
+	im_r = gal + '_offset_w1_cr_bgsub.fits'
+	im_g = gal + '_offset_m2_cr_bgsub.fits'
+	im_b = gal + '_offset_w2_cr_bgsub.fits'
 
-		vmid_list = 0.001
-		vmin_list = []
-		for i,im in enumerate([im_r, im_g, im_b]):
-			hdu_list = fits.open(im)
-			filt = sigma_clip(hdu_list[0].data, sigma=2, iters=5)
-			vmin_list.append( np.mean(filt.data[~filt.mask]) + 1.5 * np.std(filt.data[~filt.mask]) )
-			hdu_list.close()
+	vmid_list = 0.001
+	vmin_list = []
+	
+	for i,im in enumerate([im_r, im_g, im_b]):
+		hdu_list = fits.open(im)
+		filt = sigma_clip(hdu_list[0].data, sigma=2, iters=5)
+		vmin_list.append( np.mean(filt.data[~filt.mask]) + 1.5 * np.std(filt.data[~filt.mask]) )
+		hdu_list.close()
 
-		# - create fits images in same projection
-		print('* creating rgb fits cube for '+gal)
-		aplpy.make_rgb_cube([im_r, im_g, im_b],
-								gal+'_rgb_cube.fits', north=True)
-		# - make the rgb image
-		print('* creating rgb png image of '+gal)
-		aplpy.make_rgb_image(gal+'_rgb_cube.fits', gal+'_image'+'.png',
-								 stretch_r='log', stretch_g='log', stretch_b='log',
-								 vmin_r=vmin_list[0], vmin_g=vmin_list[1], vmin_b=vmin_list[2],
-								 vmid_r=vmid_list, vmid_g=vmid_list, vmid_b=vmid_list,
-								 pmax_r=99.95, pmax_g=99.9, pmax_b=99.9,
-								 make_nans_transparent=True)
+	# - create fits images in same projection
+	print('* creating rgb fits cube for '+gal)
+	aplpy.make_rgb_cube([im_r, im_g, im_b],
+							gal+'_rgb_cube.fits', north=True)
+	# - make the rgb image
+	print('* creating rgb png image of '+gal)
+	aplpy.make_rgb_image(gal+'_rgb_cube.fits', gal+'_image'+'.png',
+							 stretch_r='log', stretch_g='log', stretch_b='log',
+							 vmin_r=vmin_list[0], vmin_g=vmin_list[1], vmin_b=vmin_list[2],
+							 vmid_r=vmid_list, vmid_g=vmid_list, vmid_b=vmid_list,
+							 pmax_r=99.95, pmax_g=99.9, pmax_b=99.9,
+							 make_nans_transparent=True)
+
+
+def filter_check():
+	filt_list = ['w2','m2','w1']
+	filter_list = list()
+	for filt in filt_list:
+		filt_data_exists = glob.glob('*/*/image/*'+filt+'*')
+		if len(filt_data_exists) > 0:
+			filter_list.append(filt)
+
+	if len(filter_list) == 1:
+		print('Data only found for filter '+filter_list[0]+", processing will not continue. Please check to make sure data for each filter exists.")
+		print('Moving on to next object...')
+		unprocessed_gals.write(gal + "\n")
+		why_bypass.write(gal+': missing data for some filters'+'\n')
+		continue
+	elif len(filter_list) == 2:
+		print('Data only found for filters '+filter_list[0]+" and "+filter_list[1]+", processing will not continue. Please check to make sure data for each filter exists.")
+		print('Moving on to next object...')
+		unprocessed_gals.write(gal+'\n')
+		why_bypass.write(gal+': missing data for some filters'+'\n')
+		continue
+
+
+
+def process_progress():
+	#this piece is still useful, but is going to have to change since obslist will be created differently
+	processing_done = glob.glob(gal+"_image.png")
+	if len(processing_done) > 0 and len(obslist) == len(current_obslist):
+		print("* Nothing new to add for "+gal+". Moving along...")
+		return True			 
+	else:
+		#remove the png? and some other files? what else will be frustrating to overwrite?
+		os.remove(gal+'_image.png')
+		image_processing()
+
+
+
+
+
+def data_download():
+	unprocessed_gals = open('unprocessed_gals', 'w+')
+	why_bypass = open('why_bypass', 'w+')
+
+	swift_db_query() #script written to query the swift database, search the sotbackend fillin subdatabase and return all the targets that are marked as done and have
+	ready_to_process = np.loadtxt('ready_to_process_galaxies.txt', dtype = 'str').tolist()
+
+	for gal in ready_to_process: #gal_list now comes from swift_db_query, so is it better to just put the code in here??
+		os.chdir('..') #cd into full lvls directory so it doesn't make everything in your python directory
+
+		query_heasarc(gal) #only needs to accept one string object since we're iterating through list right now
+
+		obs_folder_exists = glob.glob("00*")
+		with open('heasarc_obs.dat', 'r') as fh:
+			rows_list = fh.readlines()
+
+		total_obs = len(rows_list) - 4
+		if total_obs != len(obs_folder_exists): #i want this to happen if there is a new download, but also if this object is new then download has to be run
+												#so if this needs to be run for a new object too, where do I account for that too??
+
+
+			download_heasarc('heasarc_obs.dat')
+		else:
+			print("* Download already done")
+			print("* No new observations to download")
+			print('* Moving on to processing...')
+			#uh hang on can I just stick the processing function into this?? OH YOU FOOL THIS IS THE USE OF FUNCTIONIZING
+			#YOU MADMAN
+			#skip on here to next shenanigans
+
+
+		progress = process_progress()
+		if progress = True:
+			continue
+
+
+
+		#processing progress function and filter checks still need to go somewhere in here, but they can be jumped to instead of being part of the bigger function
+		
+
+		image_processing()
 
 	unprocessed_gals.close()
 
+	why_bypass.close()
+
+
+
 auto_color()
-
-'''
-latest issues:
-image is produced, but background levels are still very obviously visible
-	- lea has something called offset_mosaic in her repository
-		- takes background 
-
-'''
-
-#at this point the script should be able to access browse_extract, return the data file made, open a download script, open a data.list
-#for the entries in the list, store the three columns to three diff variables
-#write the wget_uvot and auxil statements, then write them into the download script and data.list
