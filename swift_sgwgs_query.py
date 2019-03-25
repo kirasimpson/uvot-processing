9192ff0222f5bf83bf827162749858176ebf8a93
kirasimpson <kira.simpson1984@gmail.com>
2019-03-25 14:55:09 -0400

    Update swift_sgwgs_query.py

--- swift_sgwgs_query.py
+++ swift_sgwgs_query.py
@@ -5,7 +5,11 @@ import numpy as np
 
 def obs_progress_query(compare_times=False):
 	'''
-	Query Swift database for finished targets in Swift fill-in surveys
+	Execute an SQL query to the Swift backend for finished targets in SGWGSurvey.
+	
+	Outputs
+	-------
+	Writes out a file sgwgs_done.list that contains the names and IDs of fully observed targets
 	'''
 
 	BackendDB = mysql.CalendarDB()
@@ -13,10 +17,10 @@ def obs_progress_query(compare_times=False):
 	command = "SELECT `targname`,`targid` FROM fillin_targets WHERE `comment` LIKE '% Aaron Tohuvavohu%' AND `done` = 1"
 
 	results = BackendDB.cur.execute(command)
-
+	
 	Targ_names_IDs = BackendDB.cur.fetchall()
 
-
+	
 	file = open("sgwgs_done.list", "w+")
 	for i in range(len(Targ_names_IDs)):
 		file.write(str(Targ_names_IDs[i][1])  + " " + str(Targ_names_IDs[i][0]) + "\n")
